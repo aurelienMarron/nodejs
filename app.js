e09fefa0db2ceb8915f5f139deeeb69a8178f1ca
aurelien.marron <Calambrit14>
2021-10-06 11:29:40 +0200

    test core autorisation encore et encore

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ app.use(express.json())
 app.use(express.static('public'))
 app.use(connectLivereload());
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Credentials','true')
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", 'GET, POST, OPTIONS, PUT, DELETE');
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     next();
 });
 // app.options('/api/*', function (req, res) {
