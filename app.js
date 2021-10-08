99c939fa201be2c21e7cd5decf0bbf7d1ab4018e
aurelien.marron <Calambrit14>
2021-10-08 09:13:39 +0200

    test core autorisation pour l'api déployée 3

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.json())
 app.use(express.static('public'))
 app.use(connectLivereload());
 app.use(function(req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "https://aurel-node-movies.herokuapp.com/");
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080/");
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
